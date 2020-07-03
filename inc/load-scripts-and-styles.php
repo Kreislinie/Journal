3fdfba9bc6f87082a02e6a283d1420994120f5f6
Simon Mettler <admin@simonmettler.com>
2020-07-03 13:15:10 +0200

    update script and css links

--- inc/load-scripts-and-styles.php
+++ inc/load-scripts-and-styles.php
@@ -6,11 +6,9 @@
 
 function bj_frontend_scripts() {
 
-  wp_enqueue_style( 'bitjournal-style', get_stylesheet_directory_uri() . '/style.min.css' );
+  wp_enqueue_style( 'bitjournal-style', get_stylesheet_directory_uri() . '/style.css' );
 
-  wp_enqueue_script( 'bitjournal-custom', get_template_directory_uri() . '/js/custom.min.js', array(), null, true );
-
-  wp_enqueue_script( 'bitjournal-skip-link-focus-fix', get_template_directory_uri() . '/js/vendors.min.js', array(), null, true );
+	// wp_enqueue_script( 'bitjournal-custom', get_template_directory_uri() . '/js/custom.min.js', array(), null, true );
 
   wp_enqueue_script( 'jquery' );
 
@@ -28,7 +26,7 @@ function bj_backend_scripts() {
 
   wp_enqueue_style( 'font-awesome-default', get_template_directory_uri() . '/vendors/font-awesome/css/fontawesome.min.css' );
   wp_enqueue_style( 'font-awesome', get_template_directory_uri() . '/vendors/font-awesome/css/all.min.css' );
-  wp_enqueue_style( 'bitjournal-backend-style', get_stylesheet_directory_uri() . '/style-backend.min.css' );
+  wp_enqueue_style( 'bitjournal-backend-style', get_stylesheet_directory_uri() . '/style-backend.css' );
 
 }
 
