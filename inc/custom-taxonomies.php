c6ed711fe15a5418b5db786bb4dbd394d012085a
Simon Mettler <admin@simonmettler.com>
2020-07-05 15:31:29 +0200

    update people taxonomy

--- inc/custom-taxonomies.php
+++ inc/custom-taxonomies.php
@@ -1,14 +1,9 @@
 <?php
-/**
- * bitjournal register custom taxonomies
- * 
- * @package bitjournal
- * 
+/*
+ * Register custom taxonomies.
  */
 
-/**
- * Create and register people taxonomy
- */
+// People taxonomy
 function bj_people_taxonomy() {
 
 	$labels = array(
@@ -49,46 +44,3 @@ function bj_people_taxonomy() {
 
 add_action( 'init', 'bj_people_taxonomy' );
 
-
-/**
- * Create and register mood taxonomy
- */
-function bj_emotions_taxonomy() {
-
-	$labels = array(
-		'name'                           => 'Emotions',
-		'singular_name'                  => 'Emotion',
-		'search_items'                   => 'Search Emotions',
-		'all_items'                      => 'All Emotions',
-		'edit_item'                      => 'Edit Emotion',
-		'update_item'                    => 'Update Emotion',
-		'add_new_item'                   => 'Add New Emotion',
-		'new_item_name'                  => 'New Emotion Name',
-		'menu_name'                      => 'Emotions',
-		'view_item'                      => 'View Emotion',
-		'popular_items'                  => 'Popular Emotions',
-		'separate_items_with_commas'     => 'Separate Emotions with commas',
-		'add_or_remove_items'            => 'Add or remove Emotions',
-		'choose_from_most_used'          => 'Choose from the most used Emotions',
-		'not_found'                      => 'No Emotion found'
-	);
-
-  $args = array(
-    'hierarchical'                   => false,
-    'show_ui'                        => true,
-    'show_admin_column'              => true,
-    'query_var'                      => true,
-    'public'                         => true,
-    'show_in_nav_menus'              => true,
-    'query_var'                      => true,
-    'show_in_menu'                   => false,
-    'show_in_rest'                   => true,
-    'rewrite'                        => array( 'slug' => 'emotion' ),
-    'labels'                         => $labels
-  );
-  
-  register_taxonomy( 'emotions', array( 'entry' ), $args );
-  
-}
-
-add_action( 'init', 'bj_emotions_taxonomy' );
