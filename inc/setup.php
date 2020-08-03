34afadc54b9ef716b514e90e6186819859bca591
Simon Mettler <admin@simonmettler.com>
2020-08-03 16:16:09 +0200

    update setup function

--- inc/setup.php
+++ inc/setup.php
@@ -1,93 +1,34 @@
 <?php
-/*
- * Create needed pages on theme activation.
- */
+
 if ( isset( $_GET['activated'] ) && is_admin() ) :
 
-  $people_title = __('People', 'bitjournal');
-  $people_page_template = 'page-people.php';
-  $people_check = get_page_by_title( $people_title );
+	/*
+	 * Create needed pages on theme activation.
+	 */
+	function createPage($page_title, $page_template) {
 
-  $archive_title = __('Archive', 'bitjournal');
-  $archive_page_template = 'page-archive.php';
-  $archive_check = get_page_by_title( $archive_title );
+		$page_settings  = array(
+			'post_type' => 'page',
+			'post_title' => $page_title,
+			'post_status' => 'publish'
+		);
 
-  $tags_title = __('Tags', 'bitjournal');
-  $tags_page_template = 'page-tag.php';
-  $tags_check = get_page_by_title( $tags_title );
+		if( !isset( get_page_by_title( $page_title )->ID ) ) {
 
-  $category_title = __('Categories', 'bitjournal');
-  $category_page_template = 'page-category.php';
-  $category_check = get_page_by_title( $category_title );
+			$page = wp_insert_post( $page_settings );
 
-  /*
-   * Array for wp_insert_post 
-   * https://developer.wordpress.org/reference/functions/wp_insert_post
-   */   
-  $people_page = array(
-    'post_type' => 'page',
-    'post_title' => $people_title,
-    'post_status' => 'publish'
-  );
+			if( !empty( $page_template ) ) {
+				update_post_meta( $page, '_wp_page_template', $page_template );
+			}
 
-  $archive_page = array(
-    'post_type' => 'page',
-    'post_title' => $archive_title,
-    'post_status' => 'publish'
-  );
+		} 
 
-  $tags_page = array(
-    'post_type' => 'page',
-    'post_title' => $tags_title,
-    'post_status' => 'publish'
-  );
+	}
 
-  $category_page = array(
-    'post_type' => 'page',
-    'post_title' => $category_title,
-    'post_status' => 'publish'
-  );
-
-  // Creates page "people".
-  if( !isset( $people_check->ID ) ) {
-
-    $people_page_id = wp_insert_post( $people_page );
-
-    if( !empty( $people_page_template ) ) {
-      update_post_meta( $people_page_id, '_wp_page_template', $people_page_template );
-    }
-
-  } 
-
-  // Creates page "archive".
-  if ( !isset( $archive_check->ID ) ) {
-
-    $archive_page_id = wp_insert_post( $archive_page );
-
-    if( !empty( $archive_page_template ) ) {
-      update_post_meta( $archive_page_id, '_wp_page_template', $archive_page_template );
-    }
-  }
-
-  // Creates page "tags".
-  if ( !isset( $tags_check->ID ) ) {
-
-    $tags_page_id = wp_insert_post( $tags_page );
-
-    if( !empty( $tags_page_template ) ) {
-      update_post_meta( $tags_page_id, '_wp_page_template', $tags_page_template );
-    }
-  }
-
-  // Creates page "categories".
-  if ( !isset( $category_check->ID ) ) {
-
-    $category_page_id = wp_insert_post( $category_page );
-  
-    if( !empty( $category_page_template ) ) {
-      update_post_meta( $category_page_id, '_wp_page_template', $category_page_template );
-    }
-  }
+	createPage('People', 'page-people.php');
+	createPage('Archive', 'page-archive.php');
+	createPage('Tags', 'page-tag.php');
+	createPage('Categories', 'page-category.php');
 
   /*
    * Set permalink structure.
@@ -96,7 +37,6 @@ if ( isset( $_GET['activated'] ) && is_admin() ) :
 
   $wp_rewrite->set_permalink_structure('/%year%/%monthnum%/%day%/%postname%/'); 
 
-  // Set the option.
   update_option( "rewrite_rules", FALSE ); 
 
   // Flush the rules and tell it to write htaccess.
