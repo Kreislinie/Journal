c7181a32e771cb41057c8c7dda280f9eff3941c2
Simon Mettler <contact@simonmettler.com>
2024-06-22 14:06:59 +0200

    update packages

--- js/bj-script.js
+++ js/bj-script.js
@@ -1,1 +1,1 @@
-"use strict";jQuery(document).ready(function(n){n(".link").click(function(){return window.location=n(this).data("url"),!1}),n(".mobile-nav-wrap").on("click",function(){n(".mobile-nav").toggleClass("active"),n(".navbar-left").toggleClass("open")})});
+jQuery(document).ready(function(n){n(".link").click(function(){return window.location=n(this).data("url"),!1}),n(".mobile-nav-wrap").on("click",function(){n(".mobile-nav").toggleClass("active"),n(".navbar-left").toggleClass("open")})});
